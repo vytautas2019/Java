89bee5ebf4cb5777a03d1b0f4db20960c31f46bf
Vytautas <vytautas.1717@gmail.com>
2019-01-25 20:48:38 +0200

    fc

---
+++ Darbuotojas/src/com/company/Darbuotojas.java
@@ -0,0 +1,78 @@
+package com.company;
+
+public class Darbuotojas {
+    private int atlyginimas;
+    private int darbas;
+    private int valandos;
+    private String darbuotojoVardas;
+    private String pareigos;
+    public static int k=0;
+
+    public void setPareigos(String pareigos) {
+        this.pareigos = pareigos;
+    }
+
+    public String getPareigos() {
+        return pareigos;
+    }
+
+    public Darbuotojas() {
+        System.out.println("Sukurtas Darbuotojas: ");
+
+    }
+
+    public void setAtlyginimas(int atlyginimas) {
+        this.atlyginimas = atlyginimas;
+    }
+
+    public int getAtlyginimas() {
+        return atlyginimas;
+    }
+
+    public void setDarbas(int darbas) {
+        this.darbas = darbas;
+    }
+
+    public int getDarbas() {
+        return darbas;
+    }
+    public void gautiInfo(){
+        k++;
+        System.out.println("K: "+k+"  Darbuotojas: "+getDarbuotojoVardas()+" Atlyginimas: "+getAtlyginimas()+ " Dirbta valandu: " + getValandos());
+
+
+    }
+
+    public void setValandos(int valandos) {
+        this.valandos = valandos;
+    }
+
+    public int getValandos() {
+        return valandos;
+    }
+
+    public String getDarbuotojoVardas() {
+        return darbuotojoVardas;
+    }
+
+    public void setDarbuotojoVardas(String darbuotojoVardas) {
+        this.darbuotojoVardas = darbuotojoVardas;
+    }
+    public Darbuotojas(String darbuotojoVardas,int atlyginimas,int valandos){
+        this.darbuotojoVardas = darbuotojoVardas;
+        this.atlyginimas = atlyginimas;
+        this.valandos = valandos;
+
+    }
+    public void pridetiAlyginima(){
+        atlyginimas+=5;
+    }
+    public void pridetiDarba(int v){
+        atlyginimas+= 77;
+        valandos+=v;
+
+    }
+    public  void isvalytiK(){
+         k=0;
+    }
+}
---
+++ Darbuotojas/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Darbuotojas d1= new Darbuotojas("Valdas",1500,220);
+	d1.gautiInfo();
+        Darbuotojas d2= new Darbuotojas();
+        d1.pridetiAlyginima();
+        d1.gautiInfo();
+        d1.pridetiDarba(8);
+        d1.gautiInfo();
+        d1.isvalytiK();
+        d1.gautiInfo();
+
+    }
+}
