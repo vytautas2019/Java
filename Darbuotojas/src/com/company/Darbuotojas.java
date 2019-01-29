cc0aeea497974547efdeaf3cb68e7c5849824827
Vytautas <vytautas.1717@gmail.com>
2019-01-29 21:26:13 +0200

    Uzbaigtas

--- Darbuotojas/src/com/company/Darbuotojas.java
+++ Darbuotojas/src/com/company/Darbuotojas.java
@@ -1,55 +1,63 @@
 package com.company;
 
 public class Darbuotojas {
+    public static int k = 0;
     private int atlyginimas;
     private int darbas;
     private int valandos;
     private String darbuotojoVardas;
     private String pareigos;
-    private String p;
-    public static int k=0;
 
-    public void setPareigos(String pareigos) {
-        this.pareigos = pareigos;
-    }
-
-    public String getPareigos() {
-        return pareigos;
-    }
 
     public Darbuotojas() {
         System.out.println("Sukurtas Darbuotojas: ");
 
     }
 
-    public void setAtlyginimas(int atlyginimas) {
+    public Darbuotojas(String darbuotojoVardas, int atlyginimas, int valandos) {
+        this.darbuotojoVardas = darbuotojoVardas;
         this.atlyginimas = atlyginimas;
+        this.valandos = valandos;
+
+    }
+
+    public String getPareigos() {
+        return pareigos;
+    }
+
+    public void setPareigos(String pareigos) {
+        this.pareigos = pareigos;
     }
 
     public int getAtlyginimas() {
         return atlyginimas;
     }
 
-    public void setDarbas(int darbas) {
-        this.darbas = darbas;
+    public void setAtlyginimas(int atlyginimas) {
+        this.atlyginimas = atlyginimas;
     }
 
     public int getDarbas() {
         return darbas;
     }
-    public void gautiInfo(){
-        k++;
-        System.out.println("K: "+k+"  Darbuotojas: "+getDarbuotojoVardas()+" Atlyginimas: "+getAtlyginimas()+ " Dirbta valandu: " + getValandos());
 
-
+    public void setDarbas(int darbas) {
+        this.darbas = darbas;
     }
 
-    public void setValandos(int valandos) {
-        this.valandos = valandos;
+    public void gautiInfo() {
+        k++;
+        System.out.println("K: " + k + "  Darbuotojas: " + getDarbuotojoVardas() + " Atlyginimas: " + getAtlyginimas() + " Dirbta valandu: " + getValandos());
+
+
     }
 
     public int getValandos() {
         return valandos;
+    }
+
+    public void setValandos(int valandos) {
+        this.valandos = valandos;
     }
 
     public String getDarbuotojoVardas() {
@@ -59,21 +67,18 @@ public class Darbuotojas {
     public void setDarbuotojoVardas(String darbuotojoVardas) {
         this.darbuotojoVardas = darbuotojoVardas;
     }
-    public Darbuotojas(String darbuotojoVardas,int atlyginimas,int valandos){
-        this.darbuotojoVardas = darbuotojoVardas;
-        this.atlyginimas = atlyginimas;
-        this.valandos = valandos;
+
+    public void pridetiAlyginima() {
+        atlyginimas += 5;
+    }
+
+    public void pridetiDarba(int v) {
+        atlyginimas += 77;
+        valandos += v;
 
     }
-    public void pridetiAlyginima(){
-        atlyginimas+=5;
-    }
-    public void pridetiDarba(int v){
-        atlyginimas+= 77;
-        valandos+=v;
 
-    }
-    public  void isvalytiK(){
-         k=0;
+    public void isvalytiK() {
+        k = 0;
     }
 }
